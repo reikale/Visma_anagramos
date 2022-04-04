dd34d571c5eede4c9a63814785e650ea2e80c403
reikale <sviackeviciute@gmail.com>
2022-04-04 20:31:50 +0300

    Swap project files

--- AnagramSolver/AnagramSolver.BusinessLogic/DictionaryController.cs
+++
@@ -1,6 +0,0 @@
-namespace Visma_anagramos;
-
-public class DictionaryController
-{
-    private List<char> abecele = new List<char> { 'a', 'ą', 'b', 'c', 'č', 'd', 'e', 'ę', 'ė', 'f', 'g', 'h', 'i', 'į', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 'š', 't', 'u', 'ų', 'ū', 'v', 'z', 'ž'};
-}
